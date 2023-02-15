67d28e05359131847ee2a28d72bcd867fb072312
hamadou diallo <hamadou_diallo@yahoo.com>
2023-02-15 09:05:31 -0500

    interface / advanced Types

--- interface.ts
+++ interface.ts
@@ -35,5 +35,70 @@ class MyFriend implements Friends {
   }
 }
 
-const f = new MyFriend("bol");
-console.log("f", f.getFriends());
+// const f = new MyFriend("bol");
+
+// Intersection type=> combine type and make sure type contain the type require
+
+type Employee = {
+  name: string;
+  privileges: string[] | [];
+};
+
+type Started = {
+  startDate: Date;
+};
+
+type Combine = Employee & Started;
+
+const employee: Combine = {
+  name: "voila",
+  privileges: [],
+  startDate: new Date(),
+};
+
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+interface Horse {
+  type: "horse";
+  groundSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+const runningAnimal = (animal: Animal) => {
+  let speed: number;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.groundSpeed;
+      break;
+  }
+  console.log("speed", speed);
+};
+
+const userInput = document.getElementById("user-input");
+if (userInput) {
+  (userInput as HTMLInputElement).value = "Hi There";
+}
+
+interface ErrorObject {
+  [prop: string]: string;
+}
+
+const errorBag: ErrorObject = {
+  email: "Not a Valid email",
+};
+
+const fetchedUser = {
+  id: "u1",
+  job: {
+    title: "CEO",
+    description: "Yes",
+  },
+};
+
+console.log("fetchedUser", fetchedUser?.job?.title);
